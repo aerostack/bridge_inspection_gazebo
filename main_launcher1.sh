1c36e3120e23e9304dd8aef89874e521cc0603d5
pablo <pablo@cvar>
2021-09-27 15:04:30 +0200

    Project switched to px4

--- main_launcher1.sh
+++ main_launcher1.sh
@@ -1,145 +1,90 @@
 #!/bin/bash
 
-NUMID_DRONE=111
-DRONE_SWARM_ID=1
-MAV_NAME=hummingbird_adr
+NUMID_DRONE=1
+NETWORK_ROSCORE=$1
+SESSION=$USER
+UAV_MASS=1.5
 
-export APPLICATION_PATH=${PWD}
+mv $AEROSTACK_WORKSPACE/develIgnore $AEROSTACK_WORKSPACE/devel
+export APPLICATION_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
- 
+# Kill any previous session (-t -> target session, -a -> all other sessions )
+tmux kill-session -t $SESSION
+tmux kill-session -a
 
-#---------------------------------------------------------------------------------------------
-# INTERNAL PROCESSES
-#---------------------------------------------------------------------------------------------
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Basic Behaviors                                                                             ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Basic Behaviors" --command "bash -c \"
-roslaunch basic_quadrotor_behaviors basic_quadrotor_behaviors.launch --wait \
-    namespace:=drone$NUMID_DRONE;
-exec bash\"" ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Swarm behaviors                                                                             ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Swarm behaviors  " --command "bash -c \"
-roslaunch swarm_interaction swarm_interaction.launch --wait \
-  namespace:=drone$NUMID_DRONE;
-exec bash\"" ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Gazebo motor speed controller                                                               ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Gazebo motor speed controller"  --command "bash -c \"
-roslaunch motor_speed_controller motor_speed_controller.launch --wait \
-  namespace:=drone$NUMID_DRONE \
-  mav_name:=hummingbird;
-exec bash\"" ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Quadrotor Motion With PID Control                                                           ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Quadrotor Motion With PID Control" --command "bash -c \"
-roslaunch quadrotor_motion_with_pid_control quadrotor_motion_with_pid_control.launch --wait \
+# Create new session  (-2 allows 256 colors in the terminal, -s -> session name, -d -> not attach to the new session)
+tmux -2 new-session -d -s $SESSION
+
+# Create one window per roslaunch (-t -> target session , -n -> window name) 
+tmux new-window -t $SESSION:1 -n 'viewer + teleop'
+tmux send-keys "roslaunch alphanumeric_viewer alphanumeric_viewer.launch --wait \
+    drone_id_namespace:=drone$NUMID_DRONE \
+    my_stack_directory:=${APPLICATION_PATH}" C-m
+
+tmux split-window -t $SESSION:1
+tmux send-keys  "roslaunch keyboard_teleoperation_with_pid_control keyboard_teleoperation_with_pid_control.launch --wait \
+  drone_id_namespace:=drone$NUMID_DRONE" C-m
+            
+tmux new-window -t $SESSION:2 -n 'pixhawk interface'
+tmux send-keys "roslaunch pixhawk_interface pixhawk_interface.launch \
+--wait drone_id_namespace:=drone$NUMID_DRONE acro_mode:=false simulation_mode:=true " C-m
+
+tmux new-window -t $SESSION:3 -n 'Basic Quadrotor Behaviors'
+tmux send-keys "roslaunch basic_quadrotor_behaviors basic_quadrotor_behaviors.launch --wait \
+  namespace:=drone$NUMID_DRONE" C-m
+
+tmux new-window -t $SESSION:4 -n 'Quadrotor Motion With PID Control'
+tmux send-keys "roslaunch quadrotor_motion_with_pid_control quadrotor_motion_with_pid_control.launch --wait \
     namespace:=drone$NUMID_DRONE \
     robot_config_path:=${APPLICATION_PATH}/configs/drone$NUMID_DRONE \
-    uav_mass:=0.7;
-exec bash\""  ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Python Interpreter                                                                          ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Python Interpreter" --command "bash -c \"
-roslaunch python_based_mission_interpreter_process python_based_mission_interpreter_process.launch --wait \
+    uav_mass:=$UAV_MASS" C-m
+
+tmux new-window -t $SESSION:5 -n 'GroundTruth Gazebo'
+tmux send-keys "rosrun topic_tools relay /drone${NUMID_DRONE}/mavros/local_position/pose /drone${NUMID_DRONE}/self_localization/pose & \
+rosrun topic_tools relay drone${NUMID_DRONE}/mavros/local_position/velocity_local /drone${NUMID_DRONE}/self_localization/speed" C-m
+
+tmux new-window -t $SESSION:6 -n 'Throttle_Controller'
+tmux send-keys "roslaunch thrust2throttle_controller thrust2throttle_controller.launch --wait \
+  namespace:=drone$NUMID_DRONE \
+  uav_mass:=$UAV_MASS" C-m
+
+tmux new-window -t $SESSION:7 -n 'Python interpreter'
+tmux send-keys "roslaunch python_based_mission_interpreter_process python_based_mission_interpreter_process.launch --wait \
   drone_id_namespace:=drone$NUMID_DRONE \
   drone_id_int:=$NUMID_DRONE \
-  mission:=mission1.py \
-  mission_configuration_folder:=${APPLICATION_PATH}/configs/mission;
-exec bash\""  ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Gazebo Interface                                                                            ` \
-`#---------------------------------------------------------------------------------------------` \
-  --tab --title "Gazebo Interface" --command "bash -c \"
-roslaunch gazebo_interface gazebo_interface.launch --wait \
-    robot_namespace:=drone$NUMID_DRONE \
-    drone_id:=$DRONE_SWARM_ID \
-    mav_name:=$MAV_NAME;
-exec bash\""  ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Belief Manager                                                                              ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Belief Manager" --command "bash -c \"
-roslaunch belief_manager_process belief_manager_process.launch --wait \
-    drone_id_namespace:=drone$NUMID_DRONE \
-    drone_id:=$NUMID_DRONE \
-    config_path:=${APPLICATION_PATH}/configs/mission;
-exec bash\""  ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Common Belief Updater                                                                              ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Common Belief Updater" --command "bash -c \"
-roslaunch common_belief_updater_process common_belief_updater_process.launch --wait \
-    drone_id_namespace:=drone$NUMID_DRONE \
-    drone_id:=$NUMID_DRONE;
-exec bash\""  ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Belief Updater                                                                              ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title " Swarm Belief Updater" --command "bash -c \"
-roslaunch swarm_belief_updater_process swarm_belief_updater_process.launch --wait \
-    drone_id_namespace:=drone$NUMID_DRONE \
-    drone_id:=$NUMID_DRONE;
-exec bash\""  ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Belief Memory Viewer                                                                        ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Belief memory Viewer" --command "bash -c \"
-roslaunch belief_memory_viewer belief_memory_viewer.launch --wait \
-  robot_namespace:=drone$NUMID_DRONE \
-  drone_id:=$NUMID_DRONE;
-exec bash\""  ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Recovery Manager                                                                            ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Recovery Manager" --command "bash -c \"
-roslaunch recovery_manager_process recovery_manager_process.launch --wait \
-  robot_namespace:=drone$NUMID_DRONE \
-  drone_id:=$DRONE_SWARM_ID;
-exec bash\"" ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Safety Monitor                                                                              ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Safety Monitor" --command "bash -c \"
-roslaunch safety_monitor_process safety_monitor_process.launch --wait \
-  robot_namespace:=drone$NUMID_DRONE;
-exec bash\"" ;
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# Behavior Coordinator                                                                       ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "Behavior coordinator" --command "bash -c \" sleep 2;
-roslaunch behavior_coordinator behavior_coordinator.launch --wait \
-  robot_namespace:=drone$NUMID_DRONE \
-  catalog_path:=${APPLICATION_PATH}/config/mission/behavior_catalog.yaml;
-exec bash\""  &
-#---------------------------------------------------------------------------------------------
-# SHELL INTERFACE
-#---------------------------------------------------------------------------------------------
-gnome-terminal  \
-`#---------------------------------------------------------------------------------------------` \
-`# alphanumeric_viewer                                                                         ` \
-`#---------------------------------------------------------------------------------------------` \
---tab --title "alphanumeric_viewer"  --command "bash -c \"
-roslaunch alphanumeric_viewer alphanumeric_viewer.launch --wait \
-    drone_id_namespace:=drone$NUMID_DRONE;
-exec bash\""  &
+  my_stack_directory:=${APPLICATION_PATH} \
+  mission:=mission_pid.py \
+  mission_configuration_folder:=${APPLICATION_PATH}/configs/mission" C-m
 
-rqt_image_view /hummingbird_adr1/camera_front/image_raw/compressed
+tmux new-window -t $SESSION:8 -n 'Behavior Coordinator'
+tmux send-keys "roslaunch behavior_coordinator behavior_coordinator.launch --wait \
+  robot_namespace:=drone$NUMID_DRONE \
+  catalog_path:=${APPLICATION_PATH}/configs/mission/behavior_catalog.yaml" C-m
+
+tmux new-window -t $SESSION:10 -n 'Belief memory Viewer'
+tmux send-keys "roslaunch belief_memory_viewer belief_memory_viewer.launch --wait \
+  robot_namespace:=drone$NUMID_DRONE \
+  drone_id:=$NUMID_DRONE" C-m
+
+tmux new-window -t $SESSION:11 -n 'Belief manager'
+tmux send-keys "roslaunch belief_manager_process belief_manager_process.launch --wait \
+  drone_id_namespace:=drone$NUMID_DRONE \
+  drone_id:=$NUMID_DRONE \
+  config_path:=${APPLICATION_PATH}/configs/mission" C-m
+
+tmux new-window -t $SESSION:12 -n 'Belief Updater'
+tmux send-keys "roslaunch common_belief_updater_process common_belief_updater_process.launch --wait \
+  drone_id_namespace:=drone$NUMID_DRONE \
+  drone_id:=$NUMID_DRONE" C-m
+
+tmux new-window -t $SESSION:13 -n 'Alphanumeric Behavior Viewer'
+tmux send-keys "roslaunch alphanumeric_behavior_viewer alphanumeric_behavior_viewer.launch --wait \
+  drone_id_namespace:=drone$NUMID_DRONE \
+  drone_id:=$NUMID_DRONE \
+  catalog_path:=${APPLICATION_PATH}/configs/mission/behavior_catalog.yaml" C-m
+
+tmux new-window -t $SESSION:14 -n 'Behavior Coordinator Logger'
+tmux send-keys "roslaunch behavior_coordinator_logger behavior_coordinator_logger.launch --wait \
+  drone_id_namespace:=drone$NUMID_DRONE" C-m
+tmux attach-session -t $SESSION:1
+
